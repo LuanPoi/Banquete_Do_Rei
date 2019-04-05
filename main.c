88dc68328df6b66ed9061ecb9497fd7cd0aa5bdd
Luan Poi - Linux - Home <luan_poi@hotmail.com>
2019-04-05 16:53:10 -0300

    Avancando na thread Cozinheiro

--- main.c
+++ main.c
@@ -3,42 +3,45 @@
 #include <pthread.h>
 
 #define NUMERODECONVIDADOS 30
-
-
+#define UMTEMPO 1/60
+// Variáveis de análise.
+int tachosFeitos = 0,
+    numRefeicoes = 0,
+    numTotalRefeicoes = 0;
+// Variáveis de controle do sistema.
+pthread_mutex_t mutTacho = PTHREAD_MUTEX_INITIALIZER,
+                mutCozinheiro = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cozinheiroUP,
+                tachoUP;
+// Variáveis de controle da lógica.
+int paellasNoTacho = 0;
 
 void *print_message_function( void *ptr );
 
 void main(){
-    
+
 
     const char *messageking = "Thread rei criada!";
     const char *messagecook = "Thread cozinheiro foi criada!";
     // Variáveis de criação.
     pthread_t rei, cozinheiro, convidado[NUMERODECONVIDADOS];
     pthread_create(&rei, NULL, print_message_function, (void*) messageking);
-    
+
     pthread_create(&cozinheiro, NULL, print_message_function, (void*) messagecook);
-    
-    // Variáveis de análise.
-    int tachosFeitos = 0, numRefeicoes = 0, numTotalRefeicoes = 0;
-    // Variáveis de controle do sistema.
-    pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
-    // Variáveis de controle da lógica.
-    int paellasNoTacho = 0;
 
     //Criacao das threads convidado
     int i = 1;
     while(i <= NUMERODECONVIDADOS){
-        pthread_mutex_lock(&mut);
+        pthread_mutex_lock(&mutTacho);
         //alterar o segundo NULL para que o print esteja nele...
         pthread_create(&convidado[i], NULL, print_message_function, (void*) &convidado[i]);
         printf("Criando thread convidado %d ...\n ",i);
         i++;
-        pthread_mutex_unlock(&mut);
+        pthread_mutex_unlock(&mutTacho);
     }
 /*
     for(int k = 0; k <30; k++){
-        printf("%02x", *(((unsigned char*) &k) + i - 1));
+        printf("%02x", (((unsigned char) &k) + i - 1));
     }*/
 
     /*
@@ -75,17 +78,55 @@ void main(){
 }
 
 
-void *print_message_function( void *ptr )
-{
+void *print_message_function( void *ptr ){
     char *message;
     message = (char *) ptr;
     printf("%s \n", message);
 }
 
-
-/*
-void preparar_comida(){
-    pthread_mutex_lock (&mut);
-    printf("Preparando comida...");
+//Convidado---------------------
+void *Convidado( void *ptr){
+    while(1){
+        if(paellasNoTacho > 0){
+            servir();
+            comer();
+        }else{
+            chamar_cozinheiro();
+        }
+    }
 }
-*/  
+
+void servir(){}
+
+void comer(){}
+
+void chamar_cozinheiro(){}
+//Convidado---------------------
+
+void *Cozinheiro( void *ptr ){
+    while(1){
+        if(paellasNoTacho == 0){
+            preparar_comida();
+            encher_tacho();
+            avisar_corte();
+            voltar_cozinha();
+        }
+    }
+}
+
+void preparar_comida(){
+    printf("O Cozinheiro (Thread: %s) esta preparando o tacho...", pthread_self);
+    sleep(3*UMTEMPO);
+}
+
+void encher_tacho(){
+    pthread_mutex_lock(&mutTacho);
+    printf("O Cozinheiro (Thread: %s) esta trocando o tacho da mesa...", pthread_self);
+    sleep(2*UMTEMPO);
+    paellasNoTacho = 12;
+    pthread_mutex_unlock(&mutTacho);
+}
+
+void avisar_corte(){int pthread_cond_broadcast(pthread_cond_t *tachoUP);}
+
+void voltar_cozinha(){int pthread_cond_wait(pthread_cond_t *restrict cozinheiroUP, pthread_mutex_t *restrict mutCozinheiro);}
