6ec815ca4580289e02a1d19b32180194b50cbd32
Luan Poi - Linux - Home <luan_poi@hotmail.com>
2019-04-06 02:16:17 -0300

    Cozinheiro->(desenvolvido), Convidado->(desenvolvido).

--- main.c
+++ main.c
@@ -1,52 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <pthread.h>
+#include <semaphore.h>
 
+#define PAELLASPORTACHO 12
 #define NUMERODECONVIDADOS 30
 #define UMTEMPO 1/60
+
+// Variáveis de controle do sistema.
+pthread_mutex_t mutTacho = PTHREAD_MUTEX_INITIALIZER;
+            
+pthread_cond_t  cozinheiroCond,
+                convidadoCond;
+
+// Variáveis de controle da lógica.
+sem_t *paellasNoTacho;
+int exit = 0;
+
 // Variáveis de análise.
 int tachosFeitos = 0,
     numRefeicoes = 0,
     numTotalRefeicoes = 0;
-// Variáveis de controle do sistema.
-pthread_mutex_t mutTacho = PTHREAD_MUTEX_INITIALIZER,
-                mutCozinheiro = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t cozinheiroUP,
-                tachoUP;
-// Variáveis de controle da lógica.
-int paellasNoTacho = 0;
-
-void *print_message_function( void *ptr );
 
 void main(){
 
-
-    const char *messageking = "Thread rei criada!";
-    const char *messagecook = "Thread cozinheiro foi criada!";
     // Variáveis de criação.
-    pthread_t rei, cozinheiro, convidado[NUMERODECONVIDADOS];
-    pthread_create(&rei, NULL, print_message_function, (void*) messageking);
+    pthread_t   cozinheiro,
+                convidado[NUMERODECONVIDADOS];
 
-    pthread_create(&cozinheiro, NULL, print_message_function, (void*) messagecook);
-
-    //Criacao das threads convidado
-    int i = 1;
-    while(i <= NUMERODECONVIDADOS){
-        pthread_mutex_lock(&mutTacho);
-        //alterar o segundo NULL para que o print esteja nele...
-        pthread_create(&convidado[i], NULL, print_message_function, (void*) &convidado[i]);
-        printf("Criando thread convidado %d ...\n ",i);
-        i++;
-        pthread_mutex_unlock(&mutTacho);
-    }
-/*
-    for(int k = 0; k <30; k++){
-        printf("%02x", (((unsigned char) &k) + i - 1));
-    }*/
-
-    /*
-        Inicializar todas as threads;
-    */
+    //Criar todas as threads;   
+    pthread_create(&cozinheiro, NULL, Cozinheiro, &paellasNoTacho);
+    for(int i = 0 ; i < NUMERODECONVIDADOS ; ++i){ pthread_create(&convidado[i], NULL, Convidado, i); }
 
     //acao final do rei para medir tudo
     /*
@@ -78,55 +63,74 @@ void main(){
 }
 
 
-void *print_message_function( void *ptr ){
-    char *message;
-    message = (char *) ptr;
-    printf("%s \n", message);
-}
-
 //Convidado---------------------
-void *Convidado( void *ptr){
-    while(1){
-        if(paellasNoTacho > 0){
-            servir();
-            comer();
+void *Convidado(int threadIndice){
+    pthread_mutex_lock(&mutTacho);
+    while(!exit || sem_getvalue(paellasNoTacho, NULL) > 0){
+        if(sem_getvalue(paellasNoTacho, NULL) > 0){
+            servir(pthread_self, threadIndice);
+            pthread_mutex_unlock(&mutTacho);
+            comer(pthread_self, threadIndice);
         }else{
-            chamar_cozinheiro();
+            pthread_mutex_unlock(&mutTacho);
+            chamar_cozinheiro(pthread_self, threadIndice);
         }
+        pthread_cond_wait(&convidadoCond, NULL);
     }
+    pthread_exit(NULL);
 }
 
-void servir(){}
+void servir(int threadID, int threadIndice){
+    printf("Thread: %d (Convidado[%d]) - servindo-se...", threadID, threadIndice);
+    sleep(UMTEMPO);
+}
 
-void comer(){}
+void comer(int threadID, int threadIndice){
+    printf("Thread: %d (Convidado[%d]) - comendo...", threadID, threadIndice);
+    srand(time(NULL));
+    sleep((2 + (rand() % 4)) * UMTEMPO);
+}
 
-void chamar_cozinheiro(){}
+void chamar_cozinheiro(int threadID, int threadIndice){
+    printf("Thread: %d (Convidado[%d]) - chamando cozinheiro...", threadID, threadIndice);
+    pthread_cond_signal(&cozinheiroCond);
+}
 //Convidado---------------------
 
-void *Cozinheiro( void *ptr ){
-    while(1){
-        if(paellasNoTacho == 0){
-            preparar_comida();
-            encher_tacho();
-            avisar_corte();
-            voltar_cozinha();
+//Cozinheiro---------------------
+void *Cozinheiro(){
+    while(!exit){
+        if(sem_getvalue(paellasNoTacho, NULL) == 0){
+            preparar_comida(pthread_self);
+            encher_tacho(pthread_self);
+            avisar_corte(pthread_self);
+            voltar_cozinha(pthread_self);
         }
     }
+    pthread_exit(NULL);
 }
 
-void preparar_comida(){
-    printf("O Cozinheiro (Thread: %s) esta preparando o tacho...", pthread_self);
+void preparar_comida(int threadID){
+    printf("Thread: %d (Cozinheiro) - preparando o tacho...", threadID);
     sleep(3*UMTEMPO);
 }
 
-void encher_tacho(){
+void encher_tacho(int threadID){
     pthread_mutex_lock(&mutTacho);
-    printf("O Cozinheiro (Thread: %s) esta trocando o tacho da mesa...", pthread_self);
+    printf("Thread: %d (Cozinheiro) - trocando tacho da mesa...", threadID);
     sleep(2*UMTEMPO);
-    paellasNoTacho = 12;
+    for(int i = 0 ; i < PAELLASPORTACHO ; ++i){sem_post(paellasNoTacho);}
+    tachosFeitos++;
     pthread_mutex_unlock(&mutTacho);
 }
 
-void avisar_corte(){int pthread_cond_broadcast(pthread_cond_t *tachoUP);}
+void avisar_corte(int threadID){
+    printf("Thread: %d (Cozinheiro) - avisando corte...", threadID);
+    pthread_cond_broadcast(&convidadoCond);
+}
 
-void voltar_cozinha(){int pthread_cond_wait(pthread_cond_t *restrict cozinheiroUP, pthread_mutex_t *restrict mutCozinheiro);}
+void voltar_cozinha(int threadID){
+    printf("Thread: %d (Cozinheiro) - voltando a cozinha...", threadID);
+    pthread_cond_wait(&cozinheiroCond, NULL);
+}
+//Cozinheiro---------------------
